ce6f10e9b2dbad97600948e5a5672ede19b5f36f
Mychael M <mychaelbmenges@gmail.com>
2021-01-10 19:08:00 -0700

    added helper function to retrieve projects with project_completed as boolean

--- api/project/model.js
+++ api/project/model.js
@@ -1,7 +1,21 @@
 const db = require('../../data/dbConfig')
 
-function find() {
-  return db("projects")
+async function find() {
+  const projects = await db("projects")
+
+  console.log(projects)
+
+  if (projects[0].project_completed === 1) {
+    return {
+      ...projects[0],
+      project_completed: true,
+    };
+  } else {
+      return {
+        ...projects[0],
+        project_completed: false,
+      };
+    }
 }
 
 module.exports = {
