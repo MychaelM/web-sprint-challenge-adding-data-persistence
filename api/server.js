5641e1a5b445acec40e3d8db0714b15d8d6c682a
Mychael M <mychaelbmenges@gmail.com>
2021-01-10 18:16:43 -0700

    created express server and welcome routing

--- api/server.js
+++ api/server.js
@@ -1,1 +1,13 @@
-// build your server here
+const express = require('express')
+// const helmet = require('helmet')
+
+const welcome = require('./welcome/welcome-router.js');
+
+
+const server = express();
+
+// server.use('helmet')
+server.use(express.json());
+server.use("/api", welcome);
+
+module.exports = server;
